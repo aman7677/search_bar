c9f2a17fe58bb9ddb5025a6a695658a96ee15a0b
aman7677 <90276637+aman7677@users.noreply.github.com>
2024-01-01 19:48:38 +0530

    Add files via upload

---
+++ Ani search.js
@@ -0,0 +1,7 @@
+const searchBarContainerEl = document.querySelector(".search-bar-container")
+
+const magnifierEl = document.querySelector(".magnifier");
+
+searchBarContainerEl.addEventListener("click",()=>{
+searchBarContainerEl.classList.toggle("active");
+});
